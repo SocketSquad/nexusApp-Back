a7bc9567f16a28df3c1e9e64619db5d703713a3a
Mohamed El Morjani <elmorjanimohamed9@gmail.com>
2024-11-12 15:55:15 +0100

    NB-85: add message attachments module

---
+++ src/utils/types.ts
@@ -0,0 +1,11 @@
+export enum AttachmentType {
+  IMAGE = 'image',
+  FILE = 'file',
+}
+
+export enum AttachmentStatus {
+  PENDING = 'pending',
+  PROCESSING = 'processing',
+  COMPLETED = 'completed',
+  FAILED = 'failed',
+}
