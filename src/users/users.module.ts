b45e4f7ff3b34941a0d61bc9388498c66a619430
Mohamed El Morjani <elmorjanimohamed9@gmail.com>
2024-11-13 22:59:24 +0100

    NB-80: configure users module
    - Add User Service

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -1,13 +1,16 @@
 import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
 import { User, UserSchema } from './schemas/user.schema';
+import { Profile, ProfileSchema } from './schemas/profile.schema';
+import { UserService } from './providers/user.service';
 
 @Module({
   imports: [
     MongooseModule.forFeature([{ name: User.name, schema: UserSchema }]),
+    MongooseModule.forFeature([{ name: Profile.name, schema: ProfileSchema }]),
   ],
   controllers: [],
-  providers: [],
+  providers: [UserService],
   exports: [],
 })
 export class UsersModule {}
