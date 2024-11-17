e6e50a6ad3389a19a2c0aab3931293d852ed8493
Mohamed El Morjani <elmorjanimohamed9@gmail.com>
2024-11-17 18:25:39 +0100

    refactor(groups): update UpdateGroupDto for better data validation

--- src/groups/dtos/update-group.dto.ts
+++ src/groups/dtos/update-group.dto.ts
@@ -1,4 +1,4 @@
-import { PartialType } from "@nestjs/swagger";
-import { CreateGroupDto } from "./create-group.dto";
+import { PartialType } from '@nestjs/swagger';
+import { CreateGroupDto } from './create-group.dto';
 
 export class UpdatedGroupDto extends PartialType(CreateGroupDto) {}
