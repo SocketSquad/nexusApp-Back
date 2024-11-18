bd76709198acf8c7f6c97d8325f2ee71b3fcbaed
Rachid Echafai <rachidchaf2001@gmail.com>
2024-11-18 16:55:54 +0100

    feat: Add AppController and AppService to AppModule; update E2E tests to use beforeEach for setup

--- src/app.module.ts
+++ src/app.module.ts
@@ -12,7 +12,8 @@ import { FriendsModule } from './friends/friends.module';
 import { FriendsRequestsModule } from './friends-requests/friends-requests.module';
 import { SessionsModule } from './sessions/sessions.module';
 import { AuthModule } from './auth/auth.module';
-
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
 @Module({
   imports: [
     ConfigModule.forRoot({
@@ -31,5 +32,7 @@ import { AuthModule } from './auth/auth.module';
     SessionsModule,
     AuthModule,
   ],
+  controllers: [AppController],
+  providers: [AppService],
 })
 export class AppModule {}
--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -2,23 +2,18 @@ import { Test, TestingModule } from '@nestjs/testing';
 import { INestApplication } from '@nestjs/common';
 import * as request from 'supertest';
 import { AppModule } from './../src/app.module';
-
 describe('AppController (e2e)', () => {
   let app: INestApplication;
-
-  beforeAll(async () => {
+  beforeEach(async () => {
     const moduleFixture: TestingModule = await Test.createTestingModule({
       imports: [AppModule],
     }).compile();
-
     app = moduleFixture.createNestApplication();
     await app.init();
   });
-
   afterAll(async () => {
     await app.close();
   });
-
   it('/ (GET)', () => {
     return request(app.getHttpServer())
       .get('/')
