9428405bd4c282ec46ba9fe2ff633f9962c7e086
aymanebs <aymane.bisdaoune@gmail.com>
2024-11-19 12:16:14 +0100

    feat(attachments): implementation of the attachments module with full functionality and tests
    Details:
    
    Developed the attachments module including schema, DTOs, repository, service, and controller.
    Added CRUD operations for attachments (create, read by ID, read by message ID, query, and delete).
    Used interfaces for service and repository to ensure flexibility and dependency injection.
    Incorporated QueryAttachmentDto for advanced query filtering options.
    Added unit tests for service, and controller to verify behavior.

---
+++ src/attachments/repositories/attachments.repository.ts
@@ -0,0 +1,47 @@
+import { Injectable } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { Model } from 'mongoose';
+import { CreateAttachmentDto } from '../dtos/create-attachments.dto';
+import { QueryAttachmentDto } from '../dtos/query-attachments.dto';
+import { IAttachmentsRepository } from '../interfaces/attachments.repository.interface';
+import { Attachment } from '../schema/attachment.schema';
+
+
+@Injectable()
+export class AttachmentsRepository implements IAttachmentsRepository {
+  constructor(
+    @InjectModel(Attachment.name)
+    private attachmentModel: Model<Attachment>,
+  ) {}
+
+  async create(
+    createAttachmentDto: CreateAttachmentDto,
+    uploaderId: string,
+  ): Promise<Attachment> {
+    const attachment = new this.attachmentModel({
+      ...createAttachmentDto,
+      uploaderId,
+    });
+    return attachment.save();
+  }
+
+  async findById(id: string): Promise<Attachment> {
+    return this.attachmentModel.findById(id).exec();
+  }
+
+  async findByMessageId(messageId: string): Promise<Attachment[]> {
+    return this.attachmentModel.find({ messageId }).exec();
+  }
+
+  async find(query: QueryAttachmentDto): Promise<Attachment[]> {
+    return this.attachmentModel.find(query).exec();
+  }
+
+  async delete(id: string): Promise<Attachment> {
+    return this.attachmentModel.findByIdAndDelete(id).exec();
+  }
+
+  async deleteByMessageId(messageId: string): Promise<void> {
+    await this.attachmentModel.deleteMany({ messageId }).exec();
+  }
+}
