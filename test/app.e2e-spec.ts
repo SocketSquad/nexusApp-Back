309033ebba9518c29ccf676bd718ead02f529719
Mohamed El Morjani <elmorjanimohamed9@gmail.com>
2024-11-20 16:55:26 +0100

    test: update test configurations and http endpoints

--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -2,26 +2,24 @@ import { Test, TestingModule } from '@nestjs/testing';
 import { INestApplication } from '@nestjs/common';
 import * as request from 'supertest';
 import { AppModule } from './../src/app.module';
-
 describe('AppController (e2e)', () => {
   let app: INestApplication;
-
   beforeEach(async () => {
     const moduleFixture: TestingModule = await Test.createTestingModule({
       imports: [AppModule],
     }).compile();
-
-    app = await moduleFixture.createNestApplication();
+    app = moduleFixture.createNestApplication();
     await app.init();
-  }, 20000); // Increase timeout to 20 seconds
+  });
 
   afterAll(async () => {
-    if (app) {
-      await app.close();
-    }
+    await app.close();
   });
 
   it('/ (GET)', () => {
-    return request(app.getHttpServer()).get('/').expect(200).expect('Hello World!');
+    return request(app.getHttpServer())
+      .get('/')
+      .expect(200)
+      .expect('Hello World!');
   });
 });
